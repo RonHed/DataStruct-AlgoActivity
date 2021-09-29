b66e518c0f4ec4b39e4f8949fa2b780dfc58f6a6
Ron Hedwig Zape <84697205+RonHed@users.noreply.github.com>
2021-09-30 01:32:34 +0800

    Added Resources Week3
    DataStructuresWeeklyActivities

---
+++ DataStructureWeeklyActivities/Week3.Test/SimpleArrayExercisesTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week3;
+
+namespace Week3.Test
+{
+    [TestClass]
+    public class SimpleArrayExercisesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CountVowels CountVowels = new CountVowels();
+            string expectedResult = 18;
+            string text = new string
+            
+
+        }
+    }
+}
